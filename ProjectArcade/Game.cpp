0f86a3650a1a52892d1f44f3226b760c37d7dcf1
Mising <krystian.wojcicki03@gmail.com>
2025-02-13 19:16:27 +0100

    'Init'

---
+++ ProjectArcade/Game.cpp
@@ -0,0 +1,23 @@
+﻿#include "Game.h"
+#include <SFML/Graphics.hpp>
+
+Game::Game()
+{
+    window.create(VideoMode(WIDTH, HEIGHT), "Project Arcade");
+    score = 0;
+}
+
+void Game::Run()
+{
+    
+    while(window.isOpen())
+    {
+        window.display();
+        Event event;
+        while(window.pollEvent(event))
+        {
+            if(event.type == Event::Closed)
+                window.close();
+        }
+    }
+}
---
+++ ProjectArcade/Game.h
@@ -0,0 +1,17 @@
+﻿#pragma once
+
+#include <SFML/Graphics.hpp>
+
+using namespace sf;
+
+class Game
+{
+private:
+    int WIDTH = 800;
+    int HEIGHT = 600;
+    RenderWindow window;
+    int score = 0;
+public:
+    Game();
+    void Run();
+};
